b37faff38c3feb60aced72082452691c5143b06f
Elke Boonen <u0116591@thomasmore.be>
2022-04-18 14:50:18 +0200

    recap

--- les 09 - open.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-# python runnen vanuit een ander python script
-
-subprocess.Popen(["C:\\Users\\<je naam>\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", 
-r"C:\Users\<volledig pad naar je python file>\les 09.py"])
