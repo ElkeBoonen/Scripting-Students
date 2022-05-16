234859b8d623094a81bf23403ba8f5c984fcb226
Elke Boonen <u0116591@thomasmore.be>
2022-05-16 11:34:34 +0200

    les 16/5

---
+++ Packaging/MyFirstPackage_BC/mypackage/strings.py
@@ -0,0 +1,18 @@
+def helloworld():
+    """ Prints hello world
+    Parameters: 
+        none
+    Returns:
+        none
+    """
+    print("Hello World")
+
+def helloname(name):
+    """ Prints hello given name
+    Parameters: 
+        name (string): a string
+    Returns:
+        none
+    """
+    print("Hello",name)
+
