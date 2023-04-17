b023e0a1105627e821152a4bd7ed240cf1ec0f64
Elke Boonen <u0116591@thomasmore.be>
2023-04-17 12:12:46 +0200

    17/4

---
+++ 06 Scheduling/test.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.Popen(r"C:\Users\elkeb\AppData\Local\Programs\Python\Python37\python.exe",r"C:\Users\elkeb\OneDrive - Thomas More\GitHub\Scripting-Students\06 Scheduling\helloworld.py")
