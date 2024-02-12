0fe0226f7c3b9384ca298254c5e95f5347f93739
Elke Boonen <u0116591@thomasmore.be>
2024-02-12 09:57:57 +0100

    Here's to new beginnings!

--- 06 Scheduling/test.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.Popen(r"C:\Users\elkeb\AppData\Local\Programs\Python\Python37\python.exe",r"C:\Users\elkeb\OneDrive - Thomas More\GitHub\Scripting-Students\06 Scheduling\helloworld.py")
--- 09 Packages/DSPS-package/build/lib/mypackage/rainbow.py
+++
@@ -1,1 +0,0 @@
-# create rainbow image 
